303b82ac327dca8099f4f8d98ab7828479384767
prathmeshk-k2 <prathmeshkokare009@gmail.com>
2026-01-18 18:17:38 +0530

    Experiment 01

---
+++ conversion.js
@@ -0,0 +1,40 @@
+let num=33;
+let String_num=String(num);
+console.log(String_num , typeof(String_num));
+console.log(num , typeof(num));
+
+let a="36 abc";
+console.log(a, typeof(a));
+
+let num1=Number(a);
+console.log(num1, typeof(num1));
+
+
+//1 number --> to string
+//  string --> to number
+
+// ***************Operation ******************
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+let str1="hello";
+let  str2="Prathmesh ";
+console.log(str1+" "+str2);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + "2")
+console.log(1 + 3 +"2")
+//js  only automaticaly convert string to a numbers in arithmaticaly operations
+console.log(true);
+console.log(+true);
+console.log(+"true");
+let game=100;
+game++;
+console.log(game);
+let score=200;
+++score;
+console.log(score)
---
+++ demo01.js
@@ -0,0 +1,12 @@
+//const a=10;
+a=20;
+console.log(a)
+
+{
+    var a=10;
+  //  let ab=20;
+    console.log(a)
+
+}
+
+//console.log(ab)
